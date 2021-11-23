bb66f12efd012afae6ad462a1805ee7db4805c89
Daiane Backes <daianebackes96@gmail.com>
2021-11-22 21:13:09 -0300

    correção do exercicio 6

--- fundamentos/exercicios/ex_6.js
+++ fundamentos/exercicios/ex_6.js
@@ -1,10 +1,11 @@
 function inverso(x) {
-    if(typeof x == "boolean") {
-        return !x;
-    } else if(typeof x == "number") {
-        return 
+    if (typeof x === "boolean") {
+      return !x;
+    } else if (typeof x === "number") {
+      return x * -1;
+    } else {
+      return `boolean ou numero esperados, mas o parametro é do tipo ${typeof x}`;
     }
-}
-console.log(inverso(false));
-console.log();
-
+  }
+  
+  console.log(inverso(false));
